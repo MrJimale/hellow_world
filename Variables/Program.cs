3cb375798c8d3c3c50a86d7f0e837dd97c9ec0f1
abdulahi jimale said <abdulahijimale831@outlook.com>
2024-12-30 08:20:20 +0300

    war nio kistoo leen a bedelay

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -4,3 +4,7 @@ Console.WriteLine(magac);//soo daabicid magaca oo kali ah
 Console.WriteLine("waxaa ley dhahaa "+ magac);// concatination 
 Console.WriteLine($"waxa leytdahaa { magac}"); //interpulation
 Console.WriteLine("Wxaa leydhaa{0}", magac);// formatted sting 
+Console.WriteLine(magac);//soo daabicid magaca oo kali ah
+Console.WriteLine("waxaa ley dhahaa " + magac);// concatination 
+Console.WriteLine($"waxa leytdahaa {magac}"); //interpulation
+Console.WriteLine("Wxaa leydhaa{0}", magac);// formatted sting 
--- hellow_world/Program.cs
+++ hellow_world/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Linq.Expressions;
 Console.WriteLine("see tahay adduuuun i.a");
+// several Gancheges to be made here haah
